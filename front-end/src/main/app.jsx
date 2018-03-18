f3ef0e109e226fc8d923b1b8489e1d76e4e3100e
miibx5 <miibx5@gmail.com>
2018-03-18 17:13:13 -0300

    Criação do Menu

--- front-end/src/main/app.jsx
+++ front-end/src/main/app.jsx
@@ -3,11 +3,14 @@ import "modules/font-awesome/css/font-awesome.min.css";
 
 import React from "react";
 import Tasks from "../components/tasks/tasks"
-import About from "../components/tasks/about"
+import About from "../components/tasks/about";
+import Menu from "../components/template/menu";
 
 export default props => (
     <div className="container">
+        <Menu />
         <Tasks />
         <About />
+
     </div>
 );
