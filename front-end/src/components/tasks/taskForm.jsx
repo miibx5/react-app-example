9c0591e2c2b2072347e9f5345776806c995ef2dd
miibx5 <miibx5@gmail.com>
2018-03-18 17:49:04 -0300

    Implementação do formulario

--- front-end/src/components/tasks/taskForm.jsx
+++ front-end/src/components/tasks/taskForm.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
 export default props => (
-    <div>
-        <h1>
-            Form
-    </h1>
+    <div role="from" className="todoForm">
+        <div className="col-xl-12 col-sm-9 col-md-10">
+            <input id="description" className="form-control" placeholder="Adcione uma tarefa" />
+        </div>
+        <div className="col-xl-12 col-sm-3 col-md-2">
+            <button className="btn btn-primary">
+                <i className="fa fa-plus" />
+            </button>
+        </div>
     </div>
 );
