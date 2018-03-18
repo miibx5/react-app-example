173fe6ea8f961f0403310aff5e051881092add49
miibx5 <miibx5@gmail.com>
2018-03-18 15:39:09 -0300

    Correção dos arquivos de configurações para aplicação funcionar

--- back-end/src/api/react-app-example/react-app-example-model-config.js
+++ back-end/src/api/react-app-example/react-app-example-model-config.js
@@ -2,7 +2,7 @@ const restful = require("node-restful");
 const mongoose = restful.mongoose;
 
 const appSchema = new mongoose.Schema({
-    descriptiion: { type: String, required: true },
+    description: { type: String, required: true },
     done: { type: Boolean, required: true, default: false },
     dateCreation: { type: Date, default: Date.now }
 });
--- back-end/src/config/routes.js
+++ back-end/src/config/routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 module.exports = function (server) {
     const router = express.Router();
-    server.use("api", router);
+    server.use("/api", router);
 
     const service = require("../api/react-app-example/react-app-example-service");
     service.register(router, "/todos");
