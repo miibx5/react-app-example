4a72a31fd46b89cdde47454288bf21813fbc2361
miibx5 <miibx5@gmail.com>
2018-03-18 16:59:18 -0300

    Criação das páginas Tasks e about

--- front-end/src/main/app.jsx
+++ front-end/src/main/app.jsx
@@ -2,9 +2,12 @@ import "modules/bootstrap/dist/css/bootstrap.min.css";
 import "modules/font-awesome/css/font-awesome.min.css";
 
 import React from "react";
+import Tasks from "../components/tasks/tasks"
+import About from "../components/tasks/about"
 
 export default props => (
     <div className="container">
-        <h1>Teste</h1>
+        <Tasks />
+        <About />
     </div>
 );
