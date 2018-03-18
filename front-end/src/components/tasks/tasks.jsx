411fab63de4539b1d57504afc7fe8dd016c515e5
miibx5 <miibx5@gmail.com>
2018-03-18 17:40:42 -0300

    Criação do comepponente de formulário e lista

--- front-end/src/components/tasks/tasks.jsx
+++ front-end/src/components/tasks/tasks.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 import PageHeader from "../template/page-header"
+import TaskForm from "../tasks/taskForm";
+import TaskList from "../tasks/task-list";
 export default class Tasks extends Component {
     render() {
         return (
             <div>
                 <PageHeader name="Tarefas" small="Cadastro" />
+                <TaskForm />
+                <TaskList />
             </div>
         );
     }
