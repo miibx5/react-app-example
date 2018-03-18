33fea06d26a7d2abaaf98cd13a98013be09e9313
miibx5 <miibx5@gmail.com>
2018-03-18 18:33:31 -0300

    Criado o componente If

--- front-end/src/components/template/iconButton.jsx
+++ front-end/src/components/template/iconButton.jsx
@@ -1,15 +1,13 @@
 import React, { Component } from "react";
+import If from "../template/if"
 
-export default props => {
-    if (props.hide) {
-        return null;
-    } else {
-        return (
-            <button className={"btn btn-" + props.style} onClick={props.onClick}>
-                <i className={"fa fa-" + props.icon} />
-            </button>
-        )
-    }
+export default props => (
 
-}
+    <If test={!props.hide}>
+        <button className={"btn btn-" + props.style} onClick={props.onClick}>
+            <i className={"fa fa-" + props.icon} />
+        </button>
+    </If>
+
+);
 
