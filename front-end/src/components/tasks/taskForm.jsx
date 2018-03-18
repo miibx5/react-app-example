be10ea66e3c682cf37db920caac852c5d6a08733
miibx5 <miibx5@gmail.com>
2018-03-18 19:35:07 -0300

    Adicionando estado na aplicação

--- front-end/src/components/tasks/taskForm.jsx
+++ front-end/src/components/tasks/taskForm.jsx
@@ -5,7 +5,9 @@ import IconButton from "../template/iconButton";
 export default props => (
     <div role="from" className="todoForm">
         <Grid cols="10 9 10">
-            <input id="description" className="form-control" placeholder="Adcione uma tarefa" />
+            <input id="description" value={props.description}
+                onChange={props.handleChange}
+                className="form-control" placeholder="Adcione uma tarefa" />
         </Grid>
         <Grid cols="12 3 2">
             <IconButton style="primary" onClick={props.handleAdd} icon="plus" />
--- front-end/src/components/tasks/tasks.jsx
+++ front-end/src/components/tasks/tasks.jsx
@@ -3,19 +3,32 @@ import PageHeader from "../template/page-header"
 import TaskForm from "../tasks/taskForm";
 import TaskList from "../tasks/task-list";
 export default class Tasks extends Component {
+
     constructor(props) {
         super(props);
+        this.state = {
+            description: "", list: []
+        }
         this.handleAdd = this.handleAdd.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
+
     handleAdd() {
         console.log("add", this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            ...this.state,
+            description: event.target.value
+        })
     }
 
     render() {
         return (
             <div>
                 <PageHeader name="Tarefas" small="Cadastro" />
-                <TaskForm handleAdd={this.handleAdd} />
+                <TaskForm handleAdd={this.handleAdd} description={this.state.description} handleChange={this.handleChange} />
                 <TaskList />
             </div>
         );
